5db4721358aaf53d40f30fbfa772b1e9e5435bcd
Olayinka O <omosehintemilade@gmail.com>
2023-08-04 22:06:31 +0100

    remove material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@material-tailwind/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -8,20 +7,18 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ToastContainer
-        position="top-right"
-        autoClose={3500}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <App />
-    </ThemeProvider>
+    <ToastContainer
+      position="top-right"
+      autoClose={3500}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+    <App />
   </React.StrictMode>
 );
